b91e88633f36d1a00b566dfce47e0973178ee982
dyah ayu <1401dyah@gmail.com>
2019-08-01 13:11:37 +0700

    fixed bg login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,16 +40,13 @@
 				<form method="POST" action="{{ url('/login') }}" class="needs-validation" >
 					{{ csrf_field() }}
 
-					<div class="form-group">
-					@if(Session::has('alert'))
-
-					<div class="alert alert-danger alert-dismissible">
-					<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-					<strong> <div>{{Session::get('alert')}}</div></strong>
-
+					<!-- <div class="form-group">
+					@if(\Session::has('alert'))
+                	<div class="alert alert-danger">
+                    	<div>{{Session::get('alert')}}</div>
 					</div>
 					@endif
-					</div>
+					</div> -->
 
 
                   <div class="form-group">
@@ -60,7 +57,7 @@
 						<input type="passowrd" name="user_password" placeholder="Password"><br>
                   </div>
 
-				  <p class="small">did you forget your password? Click <span style="color: red;">here</span></p>
+				  <p class="small">did you forget your password? Click <a href="" style="color: red;">here</a></p>
 				
                   <div class="form-group">
 						<button type="submit" class="btn">Login</button>
@@ -291,16 +288,6 @@
 
 	});
 </script>
-
-<script>
-$(document).ready(function() {
-  $(".alert").hide();
-    $(".alert").fadeTo(2000, 500).slideUp(500, function() {
-      $(".alert").slideUp(500);
-    });
-});
-</script>
-
 </body>
 
 </html>
