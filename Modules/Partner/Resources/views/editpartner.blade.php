205dcdcbffd7e7315b503440b79e609f4dcec120
Mohammad Alif Maheaven Hidayatullah <alifmaheaven2001@gmail.com>
2019-08-03 20:42:39 +0700

    CRUD for t_companybod is added

--- Modules/Partner/Resources/views/editpartner.blade.php
+++ Modules/Partner/Resources/views/editpartner.blade.php
@@ -405,6 +405,87 @@
                                       </div>
                                     </div>
 
+                                    {{-- edit Data BOD --}}
+                                    <div class="modal" id="editBod" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" >
+                                            <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                <h5 class="modal-title" id="exampleModalLabel">Tambah Bod</h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                                </div>
+                                                <div class="modal-body">
+
+
+                                                <form id="form-Bod">
+                                                        <div class="form-group">
+                                                        <input type="hidden" class="form-control" id="edit_Bodarray" >
+                                                        <label> Bod Name</label>
+                                                        <div class="input-group">
+                                                            <input type="text" class="form-control" id="edit_Bodname" placeholder="Masukan Nama" name="edit_Bodname">
+                                                        </div>
+                                                        </div>
+                                                        <div class="form-group">
+                                                        <label>Position</label>
+                                                        <div class="input-group">
+                                                        
+                                                            <div class="select"  style="width:200px;">
+                                                            <select id="edit_Bodposition">
+                                                                {{-- dinamic select --}}
+                                                            </select>
+                                                            </div>
+                                                        </div>
+                                                        </div>
+                                                        
+                                                        <div class="form-group">
+                                                        <label>Birth Day</label>
+                                                        <div class="input-group">
+                                                            
+                                                            <input type="date" id="edit_Bodbirthday" name="bday">
+                                                        </div>
+                                                        </div>
+                                                    
+                                                        <div class="form-group">
+                                                        <label>Bod Phone</label>
+                                                        <div class="input-group">
+                                                            <input type="number" class="form-control" id="edit_Bodphone" placeholder="Masukan nomor telpon" name="username">
+                                                        </div>
+                                                        </div>
+
+                                                        <div class="form-group">
+                                                        <label>Email</label>
+                                                        <div class="input-group">
+                                                            <input type="email" class="form-control" id="edit_Bodemail" placeholder="Masukan Username" name="username">
+                                                        </div>
+                                                        </div>   
+                                                                    
+                                                        <div class="form-group">
+                                                        <label>Active</label>
+                                                        <div class="input-group">
+                                                            <div class="select" style="width:200px;">
+                                                                <select id="edit_Bodactif">
+                                                                    <option value="0">Non Actif</option>
+                                                                    <option value="1">Actif</option>
+                                                                </select>
+                                                                </div>
+                                                        </div>
+                                                        </div>   
+                                                    
+                                                </form>
+                                            
+                                                </div>
+                                                <div class="modal-footer">
+
+                                                <button id="editBodbutton" form="form-daftar" type="button" class="btn btn-primary">Daftar</button>
+                                                </div>
+                                            </div>
+                                            </div>
+                                        </div>
+
+
+
+
                                     <script>
                                         var Bodstable = @JSON($Bods);
                                         console.log(Bodstable);
@@ -428,7 +509,7 @@
                                             }
                                             var arrayBods = nomorbods
                                             nomorbods += 1
-                                            htmlbods += '<tr><td>'+nomorbods+'</td><td>'+data.companybod_name+'</td><td>'+companybodpositionname+'</td><td>'+data.companybod_birthday+'</td><td>'+data.companybod_phone+'</td><td>'+data.companybod_email+'</td><td>'+companybodisactive+'</td><td><button class="btn btn-primary btn-round" data-array="'+arrayBods+'" onclick="deleteBodstablerow(this);" >Delete</button><button class="btn btn-warning btn-round" >Edit</button></td></tr>'
+                                            htmlbods += '<tr><td>'+nomorbods+'</td><td>'+data.companybod_name+'</td><td>'+companybodpositionname+'</td><td>'+data.companybod_birthday+'</td><td>'+data.companybod_phone+'</td><td>'+data.companybod_email+'</td><td>'+companybodisactive+'</td><td><button class="btn btn-primary btn-round" data-array="'+arrayBods+'" onclick="deleteBodstablerow(this);" >Delete</button><button class="btn btn-warning btn-round" data-array="'+arrayBods+'" onclick="editBodstablerow(this);" data-toggle="modal" data-target="#editBod" >Edit</button></td></tr>'
                                             $("#Bodstableid").find('tbody').html(htmlbods).show();
                                             });
                                             
@@ -447,7 +528,8 @@
                                             let htmlbods = ''
                                             let nomorbods = 0
 
-                                            Bodstable.forEach(data => {
+                                            if (Bodstable.length > 0) {
+                                                Bodstable.forEach(data => {
                                             console.log(data);
                                             for (let i = 0; i < position.length; i++) {
                                                 if (position[i].id_position == data.id_position) {
@@ -461,10 +543,31 @@
                                             }
                                             var arrayBods = nomorbods
                                             nomorbods += 1
-                                            htmlbods += '<tr><td>'+nomorbods+'</td><td>'+data.companybod_name+'</td><td>'+companybodpositionname+'</td><td>'+data.companybod_birthday+'</td><td>'+data.companybod_phone+'</td><td>'+data.companybod_email+'</td><td>'+companybodisactive+'</td><td><button class="btn btn-primary btn-round" data-array="'+arrayBods+'" onclick="deleteBodstablerow(this);" >Delete</button><button class="btn btn-warning btn-round" >Edit</button></td></tr>'
-                                            $("#Bodstableid").find('tbody').html(htmlbods).show();
-                                            });  
+                                            htmlbods += '<tr><td>'+nomorbods+'</td><td>'+data.companybod_name+'</td><td>'+companybodpositionname+'</td><td>'+data.companybod_birthday+'</td><td>'+data.companybod_phone+'</td><td>'+data.companybod_email+'</td><td>'+companybodisactive+'</td><td><button class="btn btn-primary btn-round" data-array="'+arrayBods+'" onclick="deleteBodstablerow(this);" >Delete</button><button class="btn btn-warning btn-round" data-array="'+arrayBods+'" onclick="editBodstablerow(this);" data-toggle="modal" data-target="#editBod" >Edit</button></td></tr>'
+                                             $("#Bodstableid").find('tbody').html(htmlbods).show();
+                                            }); 
+                                            } else{
+                                                htmlbods += '<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>'
+                                             $("#Bodstableid").find('tbody').html(htmlbods).show();
                                             }
+                                             
+                                            }
+                                        
+                                            
+                                            function editBodstablerow(data){     
+                                           // Bodstable.splice($(data).data('array'), 1);     
+                                            console.log(Bodstable);
+
+                                            $('#editBod').find('.modal-body').find("#edit_Bodarray").val($(data).data('array'));
+                                            $('#editBod').find('.modal-body').find("#edit_Bodname").val(Bodstable[$(data).data('array')].companybod_name);
+                                            $('#editBod').find('.modal-body').find("#edit_Bodposition").val(Bodstable[$(data).data('array')].id_position);
+                                            $('#editBod').find('.modal-body').find("#edit_Bodbirthday").val(Bodstable[$(data).data('array')].companybod_birthday);  
+                                            $('#editBod').find('.modal-body').find("#edit_Bodphone").val(Bodstable[$(data).data('array')].companybod_phone);
+                                            $('#editBod').find('.modal-body').find("#edit_Bodemail").val(Bodstable[$(data).data('array')].companybod_email);
+                                            $('#editBod').find('.modal-body').find("#edit_Bodactif").val(Bodstable[$(data).data('array')].is_active);
+
+                                            }
+                                        
 
 
                                     $(document).ready(function() {
@@ -515,7 +618,7 @@
                                             }
                                             var arrayBods = nomorbods
                                             nomorbods += 1
-                                            htmlbods += '<tr><td>'+nomorbods+'</td><td>'+data.companybod_name+'</td><td>'+companybodpositionname+'</td><td>'+data.companybod_birthday+'</td><td>'+data.companybod_phone+'</td><td>'+data.companybod_email+'</td><td>'+companybodisactive+'</td><td><button class="btn btn-primary btn-round" data-array="'+arrayBods+'" onclick="deleteBodstablerow(this);" >Delete</button><button class="btn btn-warning btn-round" >Edit</button></td></tr>'
+                                            htmlbods += '<tr><td>'+nomorbods+'</td><td>'+data.companybod_name+'</td><td>'+companybodpositionname+'</td><td>'+data.companybod_birthday+'</td><td>'+data.companybod_phone+'</td><td>'+data.companybod_email+'</td><td>'+companybodisactive+'</td><td><button class="btn btn-primary btn-round" data-array="'+arrayBods+'" onclick="deleteBodstablerow(this);" >Delete</button><button class="btn btn-warning btn-round" data-array="'+arrayBods+'" onclick="editBodstablerow(this);" data-toggle="modal" data-target="#editBod" >Edit</button></td></tr>'
                                             $("#Bodstableid").find('tbody').html(htmlbods).show();
 
                                           
@@ -526,15 +629,78 @@
                                         $("#addBod").hide();
                                         $('#addBod').modal('hide');
 	                                         });
+
+
+
+                                        $('#editBodbutton').click(function(e){ //on add input button click
+                                		e.preventDefault();
+                        
+                                        //array 
+                                        var editbodarray = document.getElementById('edit_Bodarray').value
+                                        //Bodname
+                                        var editBodname = document.getElementById('edit_Bodname').value
+                                        //posision select
+                                        var editBodposition = document.getElementById('edit_Bodposition')
+                                        var optionBodPosition = editBodposition.options[editBodposition.selectedIndex].value;
+                                        //bodbirthday
+                                        var editBodbirthday = document.getElementById('edit_Bodbirthday').value
+                                        //bodphone
+                                        var editBodphone = document.getElementById('edit_Bodphone').value
+                                        //bodemail
+                                        var editBodemail = document.getElementById('edit_Bodemail').value
+                                        //bodactive
+                                        var editBodactif = document.getElementById('edit_Bodactif')
+                                        var optionBodactif = editBodactif.options[editBodactif.selectedIndex].value;
+
+                                        Bodstable[editbodarray].companybod_name = editBodname
+                                        Bodstable[editbodarray].id_position = optionBodPosition
+                                        Bodstable[editbodarray].companybod_birthday = editBodbirthday
+                                        Bodstable[editbodarray].companybod_phone = editBodphone
+                                        Bodstable[editbodarray].companybod_email = editBodemail
+                                        Bodstable[editbodarray].is_active = optionBodactif
+                                        
+                                        
+                                        console.log(Bodstable);
+
+                                            let htmlbods = ''
+                                            let nomorbods = 0
+
+                                            Bodstable.forEach(data => {
+                                            console.log(data);
+                                            for (let i = 0; i < position.length; i++) {
+                                                if (position[i].id_position == data.id_position) {
+                                                    var companybodpositionname = position[i].position_name
+                                                }
+                                            }
+                                            if (data.is_active == 1) {
+                                                var companybodisactive = "aktif"
+                                            } else{
+                                                var companybodisactive = "non aktif"
+                                            }
+                                            var arrayBods = nomorbods
+                                            nomorbods += 1
+                                            htmlbods += '<tr><td>'+nomorbods+'</td><td>'+data.companybod_name+'</td><td>'+companybodpositionname+'</td><td>'+data.companybod_birthday+'</td><td>'+data.companybod_phone+'</td><td>'+data.companybod_email+'</td><td>'+companybodisactive+'</td><td><button class="btn btn-primary btn-round" data-array="'+arrayBods+'" onclick="deleteBodstablerow(this);" >Delete</button><button class="btn btn-warning btn-round" data-array="'+arrayBods+'" onclick="editBodstablerow(this);" data-toggle="modal" data-target="#editBod" >Edit</button></td></tr>'
+                                            $("#Bodstableid").find('tbody').html(htmlbods).show();
+
+                                          
+                                        });
+                                        jQuery.noConflict();
+                                        $("#editBod").removeClass("in");
+                                        $(".modal-backdrop").remove();
+                                        $("#editBod").hide();
+                                        $('#editBod').modal('hide');
+	                                         });
+
+
                                             });
                                     </script>
 
                                     <script>
-
                                     var position = @JSON($Positions);
                                         position.forEach(data => {
                                             console.log(data);
                                             $('<option value="'+data.id_position+'">'+data.position_name+'</option>').appendTo('#add_Bodposition');
+                                            $('<option value="'+data.id_position+'">'+data.position_name+'</option>').appendTo('#edit_Bodposition');
                                         });
                                     </script>
 
