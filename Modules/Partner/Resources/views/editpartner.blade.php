37dd47de35b93cb9a863036d18281d22663b4c2c
Mohammad Alif Maheaven Hidayatullah <alifmaheaven2001@gmail.com>
2019-08-07 17:12:38 +0700

    fixing modal succes

--- Modules/Partner/Resources/views/editpartner.blade.php
+++ Modules/Partner/Resources/views/editpartner.blade.php
@@ -1155,9 +1155,6 @@
 <div class="modal" id="succesafterclick" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" >
         <div class="modal-dialog" role="document">
                 <div class="modal-content">
-                        <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal" aria-label=""><span>×</span></button>
-                         </div>
                         
                         <div class="modal-body">
                            
