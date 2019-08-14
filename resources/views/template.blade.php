01f8e840a57f702d9f80fe996297054c51a37d29
dyah ayu <1401dyah@gmail.com>
2019-08-14 17:21:13 +0700

    add navbar and icon

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="utf-8" />
 	<link rel="apple-touch-icon" sizes="76x76" href="{{ URL::asset('img/apple-icon.png') }}">
-	<link rel="icon" type="image/png" href="{{ URL::asset('img/dyah/logo2-white.png') }}">
+	<link rel="icon" type="image/png" href="{{ URL::asset('img/dyah/logo3.png') }}">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<title>
 	
