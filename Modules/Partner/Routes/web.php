44acb32356e704b66c8f2920ba941e44ce4386bd
dyah ayu <1401dyah@gmail.com>
2019-08-15 14:21:21 +0700

    landing page

--- Modules/Partner/Routes/web.php
+++ Modules/Partner/Routes/web.php
@@ -16,4 +16,5 @@ Route::prefix('partner')->group(function() {
     Route::get('/update', 'PartnerController@getres_partner');
     Route::post('/update', 'PartnerController@add_datadetail');
     Route::get('/json', 'PartnerController@json');
+    Route::view('/halo','partner::homepage');
 });
