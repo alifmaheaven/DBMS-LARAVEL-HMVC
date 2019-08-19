6c6abb3405173af343c714fdc2c579f02112b81a
Mohammad Alif Maheaven Hidayatullah <alifmaheaven2001@gmail.com>
2019-08-19 18:19:29 +0700

    adding shorting on datatable

--- Modules/Partner/Resources/views/layouts/template.blade.php
+++ Modules/Partner/Resources/views/layouts/template.blade.php
@@ -19,6 +19,11 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 	
+{{-- datatables style --}}
+<script src="//code.jquery.com/jquery-1.12.3.js"></script>
+	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.18/datatables.min.css"/>
+	<script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.18/datatables.min.js"></script>
+
 	<!-- CSS Files -->
 	<link href="{{ URL::asset('css/material-dashboard.css?v=2.1.1') }}" rel="stylesheet" />
 	<!-- CSS Just for demo purpose, don't include it in your project -->
@@ -80,6 +85,7 @@
 
 <!-- DataTables -->
 {{-- <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script> --}}
+
 <script src="{{ URL::asset('js/jquery.dataTables.min.js')}}"></script>
 <script src="{{ URL::asset('js/dataTables.rowsGroup.js')}}"></script>
 
